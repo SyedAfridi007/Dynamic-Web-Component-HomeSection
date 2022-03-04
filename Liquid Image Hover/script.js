d1ffe23fed61998dcb1c952cc4e46742668247a3
Md. Syed Afridi <83403859+SyedAfridi007@users.noreply.github.com>
2022-03-04 22:35:08 +0600

    Add files via upload

---
+++ Liquid Image Hover/script.js
@@ -0,0 +1,25 @@
+//https://github.com/robin-dela/hover-effect/
+var h1 = new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.2,
+    image1: './01.png',
+    image2: './02.png',
+    displacementImage: './Distortion/4.png',
+    imagesRatio: 380 / 300,
+    //speedIn:2,
+    //speedOut:5
+});
+
+var h1 = new hoverEffect({
+    parent: document.querySelector('.distortion2'),
+    intensity: 0.2,
+    image1: './s21.jpeg',
+    image2: './s22.jpg',
+    displacementImage: './Distortion/4.png',
+    imagesRatio: 800 / 520,
+    //speedIn:2,
+    //speedOut:5
+});
+
+
+
